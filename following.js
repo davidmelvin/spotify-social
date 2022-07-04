8d5260818f39b1d01fba8cf8f5ea5eb772d37384
David Melvin <davidmelvinjr@gmail.com>
2022-07-03 23:24:00 -0400

    split artists and users i follow

--- following.js
+++ following.js
@@ -14,26 +14,34 @@ async function main () {
     spotifyAPI.setAccessToken(tokenResponse.body.accessToken)
 
     
-    const other = await buddyList.getFollowedAccountsForUserID(accessToken, "fzzfawpi8ustfwtikooal0ijm");
+    const other = await buddyList.getFollowedAccountsForUserID(accessToken, "pranavjain97");
     const me = await buddyList.getFollowedAccountsForUserID(accessToken, "kf4ls52nbna2ooyvj9k2ixzgb");
+
+    // console.log(other)
+    
+    const artistsIFollow = me.profiles.filter(function (profile) {
+        return profile.uri.startsWith("spotify:artist")
+    });
+
+    const friendsIFollow = me.profiles.filter(function (profile) {
+        return profile.uri.startsWith("spotify:user")
+    });
+
+    // console.log(artistsIFollow.length)
+    // for (const profile of friendsIFollow) {
+    //     console.log(profile.uri)
+    // }
 
     const overlap = other.profiles.filter(function(account) {
         return account.is_following;
     })
 
-    // console.log(overlap);
+    console.log(overlap);
     console.log(`we both follow ${overlap.length} users`)
     for (const account of overlap) {
         console.log(account.name)
     }
-    // const myArtists = await spotifyAPI.getFollowedArtists({limit: 1}).then(
-    //     function (data) {
-    //         return data.body.artists.items
-    //     },
-    //     function (err) {
-    //         console.error(err);
-    //     }
-    // )
+
 
 }
 
