e474c0eadae6b884276a000e0005bce919cb8cf7
David Melvin <davidmelvinjr@gmail.com>
2022-07-04 02:20:16 -0400

    basic functionality done

--- following.js
+++ following.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const fs = require("fs");
 const utils = require("./utils");
 
+const sharedArtistsFileLocation = __dirname + "/data/sharedArtists.json";
 const myFollowedProfilesFileLocation =
   __dirname + "/data/myFollowedProfiles.json";
 
@@ -63,13 +64,17 @@ function getMyArtistData() {
 }
 
 async function getAndSaveProfilesFollowedByMyFriends(friendData, accessToken) {
-  for (const profile of friendData) {
-    console.log(`Getting profiles followed by ${profile.name}...`);
+  const friendCount = Object.keys(friendData).length;
+  let friendNum = 0;
+  for (const [userID, profile] of Object.entries(friendData)) {
+    console.log(
+      `${friendNum}/${friendCount} Getting profiles followed by ${profile.name}...`
+    );
     const profilesFollowedByFriend =
-      await buddyList.getFollowedAccountsForUserID(accessToken, profile.userID);
+      await buddyList.getFollowedAccountsForUserID(accessToken, userID);
 
     fs.writeFile(
-      myFriendsDataFolderLocation + profile.userID + ".json",
+      myFriendsDataFolderLocation + userID + ".json",
       JSON.stringify(profilesFollowedByFriend.profiles, null, 2),
       function (err) {
         if (err) {
@@ -78,7 +83,9 @@ async function getAndSaveProfilesFollowedByMyFriends(friendData, accessToken) {
       }
     );
 
-    await new Promise((r) => setTimeout(r, 100));
+    friendNum += 1;
+
+    await new Promise((r) => setTimeout(r, 50));
   }
 }
 
@@ -89,9 +96,11 @@ function getFriendFileData() {
   utils.readFiles(
     myFriendsDataFolderLocation,
     function (filename, content) {
-      key = filename.split(".")[0];
-      friendsFollowedProfiles[key] = JSON.parse(content);
-      //   console.log(friendsFollowedProfiles[key].length);
+      if (content != "undefined") {
+        key = filename.split(".")[0];
+        friendsFollowedProfiles[key] = JSON.parse(content);
+        //   console.log(friendsFollowedProfiles[key].length);
+      }
     },
     console.err
   );
@@ -101,11 +110,19 @@ function getFriendFileData() {
 
 function listFriendsbyArtist(friendData, friendsFollowedProfiles) {
   sharedArtists = {};
-  for (const [friendUserID, friendsProfiles] of Object.entries(
+  for (const [friendUserID, followedProfiles] of Object.entries(
     friendsFollowedProfiles
   )) {
-    for (const profile of friendsProfiles) {
-      friendName = friendData[friendUserID].name;
+    if (!(friendUserID in friendData)) {
+      console.log("missing friend user id, ", friendUserID);
+      break;
+    }
+    if (friendUserID == "1297443575") {
+      console.log("skipping myself");
+      continue;
+    }
+    friendName = friendData[friendUserID].name;
+    for (const profile of followedProfiles) {
       if (profile.is_following && profile.uri.startsWith("spotify:artist")) {
         if (profile.name in sharedArtists) {
           sharedArtists[profile.name].push(friendName);
@@ -119,50 +136,41 @@ function listFriendsbyArtist(friendData, friendsFollowedProfiles) {
   return sharedArtists;
 }
 async function main() {
-  //   const spDcCookie = process.env.SP_DC_COOKIE;
-  //   const { accessToken } = await buddyList.getWebAccessToken(spDcCookie);
+  const spDcCookie = process.env.SP_DC_COOKIE;
+  const { accessToken } = await buddyList.getWebAccessToken(spDcCookie);
 
-  //   const spotifyAPI = buddyList.wrapWebApi(
-  //     new SpotifyWebApi({ spDcCookie: process.env.SP_DC_COOKIE })
-  //   );
-  //   const tokenResponse = await spotifyAPI.getWebAccessToken();
-  //   spotifyAPI.setAccessToken(tokenResponse.body.accessToken);
+  const spotifyAPI = buddyList.wrapWebApi(
+    new SpotifyWebApi({ spDcCookie: process.env.SP_DC_COOKIE })
+  );
+  const tokenResponse = await spotifyAPI.getWebAccessToken();
+  spotifyAPI.setAccessToken(tokenResponse.body.accessToken);
 
   //   await saveMyFollowedProfiles(accessToken);
   const myFriendData = getMyFriendData();
-  //   console.log(myFriendData.length);
 
   //   const myArtistData = getMyArtistData();
   //   console.log(myArtistData.length);
 
   //   getAndSaveProfilesFollowedByMyFriends(myFriendData, accessToken);
   const friendsFollowedProfiles = getFriendFileData();
-  //   console.log(Object.keys(friendsFollowedProfiles));
+  //   //   console.log(Object.keys(friendsFollowedProfiles));
 
   const sharedArtists = listFriendsbyArtist(
     myFriendData,
     friendsFollowedProfiles
   );
 
-  console.log(sharedArtists);
+  //   console.log(sharedArtists);
 
-  // for (const profile of friendData) {
-  //     console.log(`Getting profiles followed by: ${profile.name}...`)
-  //     const profilesFollowedByFriend = await buddyList.getFollowedAccountsForUserID(accessToken, profile.userID)
-
-  //     const profilesInCommon = profilesFollowedByFriend.profiles.filter(function (account) {
-  //         return account.is_following;
-  //     })
-  //     const artistsInCommon = profilesInCommon.filter(function (profile) {
-  //         return profile.uri.startsWith("spotify:artist")
-  //     });
-  //     const friendsInCommon = profilesInCommon.filter(function (profile) {
-  //         return profile.uri.startsWith("spotify:user")
-  //     });
-  //     console.log(`${profile.name} -- Arists in common: ${artistsInCommon.length}. Friends in common: ${friendsInCommon.length}.`)
-
-  //
-  // }
+  fs.writeFile(
+    sharedArtistsFileLocation,
+    JSON.stringify(sharedArtists, null, 2),
+    function (err) {
+      if (err) {
+        return console.error(err);
+      }
+    }
+  );
 }
 
 main();
