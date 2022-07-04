1301f52b6228a34d4c80766872568ee98830fcda
David Melvin <davidmelvinjr@gmail.com>
2022-07-03 23:45:42 -0400

    works for list of friends

--- following.js
+++ following.js
@@ -3,44 +3,59 @@ const buddyList = require('./')
 require('dotenv').config()
 
 
-async function main () {
-  const spDcCookie = process.env.SP_DC_COOKIE
+async function main() {
+    const spDcCookie = process.env.SP_DC_COOKIE
 
-  const { accessToken } = await buddyList.getWebAccessToken(spDcCookie)
-//   const friendActivity = await buddyList.getFriendActivity(accessToken)
+    const { accessToken } = await buddyList.getWebAccessToken(spDcCookie)
+    //   const friendActivity = await buddyList.getFriendActivity(accessToken)
 
     const spotifyAPI = buddyList.wrapWebApi(new SpotifyWebApi({ spDcCookie: process.env.SP_DC_COOKIE }))
     const tokenResponse = await spotifyAPI.getWebAccessToken()
     spotifyAPI.setAccessToken(tokenResponse.body.accessToken)
 
-    
-    const other = await buddyList.getFollowedAccountsForUserID(accessToken, "pranavjain97");
+    // const other = await buddyList.getFollowedAccountsForUserID(accessToken, "pranavjain97");
     const me = await buddyList.getFollowedAccountsForUserID(accessToken, "kf4ls52nbna2ooyvj9k2ixzgb");
 
-    // console.log(other)
-    
-    const artistsIFollow = me.profiles.filter(function (profile) {
-        return profile.uri.startsWith("spotify:artist")
-    });
+    // const artistsIFollow = me.profiles.filter(function (profile) {
+    //     return profile.uri.startsWith("spotify:artist")
+    // });
 
     const friendsIFollow = me.profiles.filter(function (profile) {
         return profile.uri.startsWith("spotify:user")
     });
 
-    // console.log(artistsIFollow.length)
-    // for (const profile of friendsIFollow) {
-    //     console.log(profile.uri)
-    // }
-
-    const overlap = other.profiles.filter(function(account) {
-        return account.is_following;
+    const friendData = friendsIFollow.map(function (profile) {
+        id = profile.uri.split(":").pop()
+        return { ...profile, userID: id }
     })
 
-    console.log(overlap);
-    console.log(`we both follow ${overlap.length} users`)
-    for (const account of overlap) {
-        console.log(account.name)
+    // console.log(friendData)
+
+    for (const profile of friendData) {
+        console.log(`Getting profiles followed by: ${profile.name}...`)
+        const profilesFollowedByFriend = await buddyList.getFollowedAccountsForUserID(accessToken, profile.userID)
+
+        const profilesInCommon = profilesFollowedByFriend.profiles.filter(function (account) {
+            return account.is_following;
+        })
+        const artistsInCommon = profilesInCommon.filter(function (profile) {
+            return profile.uri.startsWith("spotify:artist")
+        });
+        const friendsInCommon = profilesInCommon.filter(function (profile) {
+            return profile.uri.startsWith("spotify:user")
+        });
+        console.log(`${profile.name} -- Arists in common: ${artistsInCommon.length}. Friends in common: ${friendsInCommon.length}.`)
+        
+        await new Promise(r => setTimeout(r, 2000));
     }
+
+
+
+    // console.log(overlap);
+    // console.log(`we both follow ${overlap.length} users`)
+    // for (const account of overlap) {
+    //     console.log(account.name)
+    // }
 
 
 }
