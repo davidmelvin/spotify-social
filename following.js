022707446b24cb5ff5736ba0daffbf81fccea50b
David Melvin <davidmelvinjr@gmail.com>
2022-07-03 22:59:57 -0400

    get my followed artists

--- following.js
+++ following.js
@@ -1,3 +1,4 @@
+const SpotifyWebApi = require('spotify-web-api-node');
 const buddyList = require('./')
 require('dotenv').config()
 
@@ -8,10 +9,23 @@ async function main () {
   const { accessToken } = await buddyList.getWebAccessToken(spDcCookie)
 //   const friendActivity = await buddyList.getFriendActivity(accessToken)
 
+    const spotifyAPI = buddyList.wrapWebApi(new SpotifyWebApi({ spDcCookie: process.env.SP_DC_COOKIE }))
+    const tokenResponse = await spotifyAPI.getWebAccessToken()
+    spotifyAPI.setAccessToken(tokenResponse.body.accessToken)
 
+    
     const following = await buddyList.getFollowedAccountsForUserID(accessToken, "fzzfawpi8ustfwtikooal0ijm");
 
-  console.log(following)
+    const myArtists = await spotifyAPI.getFollowedArtists({limit: 1}).then(
+        function (data) {
+            return data.body.artists.items
+        },
+        function (err) {
+            console.error(err);
+        }
+    )
+
+  console.log(myArtists);
 }
 
 
