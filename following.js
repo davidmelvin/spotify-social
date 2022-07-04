7035945b1af4ae70ba7caca37251dd84480bd96f
David Melvin <davidmelvinjr@gmail.com>
2022-07-04 00:11:21 -0400

    split my friend data into a function

--- following.js
+++ following.js
@@ -1,45 +1,81 @@
-const SpotifyWebApi = require('spotify-web-api-node');
-const buddyList = require('./')
-require('dotenv').config()
+const SpotifyWebApi = require("spotify-web-api-node");
+const buddyList = require("./");
+require("dotenv").config();
+const fs = require("fs");
 
+const myFollowedProfilesFileLocation =
+  __dirname + "/data/myFollowedProfiles.json";
 
-async function main() {
-    const spDcCookie = process.env.SP_DC_COOKIE
-    const { accessToken } = await buddyList.getWebAccessToken(spDcCookie)
+async function getMyFollowedProfiles(accessToken) {
+  const me = await buddyList.getFollowedAccountsForUserID(
+    accessToken,
+    "kf4ls52nbna2ooyvj9k2ixzgb"
+  );
 
-    const spotifyAPI = buddyList.wrapWebApi(new SpotifyWebApi({ spDcCookie: process.env.SP_DC_COOKIE }))
-    const tokenResponse = await spotifyAPI.getWebAccessToken()
-    spotifyAPI.setAccessToken(tokenResponse.body.accessToken)
-
-    const me = await buddyList.getFollowedAccountsForUserID(accessToken, "kf4ls52nbna2ooyvj9k2ixzgb");
-
-    const friendsIFollow = me.profiles.filter(function (profile) {
-        return profile.uri.startsWith("spotify:user")
-    });
-
-    const friendData = friendsIFollow.map(function (profile) {
-        id = profile.uri.split(":").pop()
-        return { ...profile, userID: id }
-    })
-
-    for (const profile of friendData) {
-        console.log(`Getting profiles followed by: ${profile.name}...`)
-        const profilesFollowedByFriend = await buddyList.getFollowedAccountsForUserID(accessToken, profile.userID)
-
-        const profilesInCommon = profilesFollowedByFriend.profiles.filter(function (account) {
-            return account.is_following;
-        })
-        const artistsInCommon = profilesInCommon.filter(function (profile) {
-            return profile.uri.startsWith("spotify:artist")
-        });
-        const friendsInCommon = profilesInCommon.filter(function (profile) {
-            return profile.uri.startsWith("spotify:user")
-        });
-        console.log(`${profile.name} -- Arists in common: ${artistsInCommon.length}. Friends in common: ${friendsInCommon.length}.`)
-        
-        await new Promise(r => setTimeout(r, 2000));
+  fs.writeFile(
+    myFollowedProfilesFileLocation,
+    JSON.stringify(me, null, 2),
+    function (err) {
+      if (err) {
+        return console.error(err);
+      }
     }
-
+  );
 }
 
-main()
+function getMyFriendData() {
+  const rawData = fs.readFileSync(myFollowedProfilesFileLocation);
+  let friendData = JSON.parse(rawData).profiles;
+
+  const friendsIFollow = friendData.filter(function (profile) {
+    return profile.uri.startsWith("spotify:user");
+  });
+
+  friendDataAugmented = friendsIFollow.map(function (profile) {
+    id = profile.uri.split(":").pop();
+    return { ...profile, userID: id };
+  });
+
+  return friendDataAugmented;
+}
+async function main() {
+  const spDcCookie = process.env.SP_DC_COOKIE;
+  const { accessToken } = await buddyList.getWebAccessToken(spDcCookie);
+
+  const spotifyAPI = buddyList.wrapWebApi(
+    new SpotifyWebApi({ spDcCookie: process.env.SP_DC_COOKIE })
+  );
+  const tokenResponse = await spotifyAPI.getWebAccessToken();
+  spotifyAPI.setAccessToken(tokenResponse.body.accessToken);
+
+  //   await getMyFollowedProfiles(accessToken);
+  const myFriendData = getMyFriendData();
+  console.log(myFriendData.length);
+
+  //   for (const profile of friendData) {
+  //     console.log(`Getting profiles followed by: ${profile.name}...`);
+  //     const profilesFollowedByFriend =
+  //       await buddyList.getFollowedAccountsForUserID(accessToken, profile.userID);
+
+  //   }
+
+  // for (const profile of friendData) {
+  //     console.log(`Getting profiles followed by: ${profile.name}...`)
+  //     const profilesFollowedByFriend = await buddyList.getFollowedAccountsForUserID(accessToken, profile.userID)
+
+  //     const profilesInCommon = profilesFollowedByFriend.profiles.filter(function (account) {
+  //         return account.is_following;
+  //     })
+  //     const artistsInCommon = profilesInCommon.filter(function (profile) {
+  //         return profile.uri.startsWith("spotify:artist")
+  //     });
+  //     const friendsInCommon = profilesInCommon.filter(function (profile) {
+  //         return profile.uri.startsWith("spotify:user")
+  //     });
+  //     console.log(`${profile.name} -- Arists in common: ${artistsInCommon.length}. Friends in common: ${friendsInCommon.length}.`)
+
+  //     await new Promise(r => setTimeout(r, 2000));
+  // }
+}
+
+main();
