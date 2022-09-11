348817e9ba71800843411b05223e81d603d5bb14
David Melvin <davidmelvinjr@gmail.com>
2022-09-10 22:47:19 -0400

    install flask flask-sqlalchemy

---
+++ pydirect/friend_activity.py
@@ -0,0 +1,21 @@
+from utils.requests import get_data_from_url
+
+
+def get_friend_activity():
+    url = "https://guc-spclient.spotify.com/presence-view/v1/buddylist"
+
+    print("getting friend activity")
+
+    return get_data_from_url(url)
+
+# def get_friend_activity():
+#     url =
+#       api.getFriendActivity = function getFriendActivity(callback) {
+#     return Request.builder()
+#       .withHost("guc-spclient.spotify.com")
+#       .withPort(443)
+#       .withScheme("https")
+#       .withAuth(this.getAccessToken())
+#       .withPath("/presence-view/v1/buddylist")
+#       .build()
+#       .execute(HttpManager.get, callback);
