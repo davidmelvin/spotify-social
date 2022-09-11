c046ef0ebc2dbdd68c9c9218e55bcbd18d375903
David Melvin <davidmelvinjr@gmail.com>
2022-09-10 22:39:27 -0400

    get postgres db connection

--- pydirect/main.py
+++ pydirect/main.py
@@ -1,10 +1,10 @@
 from accounts import get_followed_accounts_of_user, test
 from friend_activity import get_friend_activity
-from utils.db import connect
+from utils.db import get_db_connection
 
 # get_followed_accounts_of_user("1297443575")
 
 # print(get_friend_activity())
 # test()
 
-connect()
+print(get_db_connection())
